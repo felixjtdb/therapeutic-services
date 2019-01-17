08acb0b727a6da29ef5f99e80a8b381667fbbd07
felixjtdb <felixjtdb@gmail.com>
2019-01-17 03:15:01 +0000

    changing css colours

--- src/Counselor.js
+++ src/Counselor.js
@@ -7,7 +7,7 @@ class Counselor extends Component {
     return (
       <div className="Container">
         <div className="Counselor_IMG_container">
-          <img src={require("./img/" + this.props.img_name + ".jpg")} alt=""/>
+          <img src={require("./img/" + this.props.img_name + ".jpg")} alt="hi"/>
           <h1 className="Counselor-header"> {this.props.name} </h1>
         </div>
         {this.props.introduction}
