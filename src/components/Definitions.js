f951ec7e19a16037c2d7e0b417557999f5427da4
felixjtdb <felixjtdb@gmail.com>
2019-06-19 00:55:00 +0100

    Changes to styling for the Definitions of therapy

--- src/components/Definitions.js
+++ src/components/Definitions.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import {
   therapyTitle,
@@ -18,25 +18,32 @@ import {
   psychodynamic,
   familySystemicTitle,
   familySystemic
-} from '../assets/introText.js'
+} from '../assets/therapyText.js'
 
 import '../css/Definitions.css'
 
-class Definitions extends Component {
-
-  render() {
+export default function Definitions() {
     return (
       <div>
 
         <h1> {therapyTitle} </h1>
         <p> {therapyPara1} </p>
-        <p> {therapyPara2} </p> <br / ><br />
+        <p> {therapyPara2} </p> <br /> 
 
         <h3> {integrativeTitle} </h3>
         <p> {integrative} </p> <br />
 
         <h3> {personCentredTitle} </h3>
-        <p> {personCentred} </p> <br />
+        <p> {personCentred} </p>
+        <ul>
+          <li>Unconditional Positive Regard, that is, a warm, positive, receptive, non-judgemental attitude towards the client.</li>
+          <li>Empathic Understanding, that is, the ability to imagine oneself in the other person’s position and to view the world from their perspective.</li>
+          <li>Congruence, or quite simply, honesty and openness.</li>
+        </ul>
+        <p>
+          The relationship between a counsellor and client is considered to be central couto the counselling as it
+          helps to inform the understanding of particular psychological difficulties as it applies to clients.
+        </p> <br />
 
         <h3> {cbtTitle} </h3>
         <p> {cbt} </p> <br />
@@ -45,7 +52,18 @@ class Definitions extends Component {
         <p> {acceptanceCommitment} </p> <br />
 
         <h3> {existentialTitle} </h3>
-        <p> {existential} </p> <br />
+        <p> {existential} </p>
+        <ul>
+          <li> All people have the capacity for self-awareness. </li>
+          <li>Each person has a unique identity that can be known only through relationships with others.</li>
+          <li>People must continually re-create themselves because life’s meaning constantly changes.</li>
+          <li>Anxiety is part of the human condition.</li>
+        </ul>
+        <p>
+          Therapists help you find meaning in the face of anxiety by choosing to think and act responsibly and by
+          confronting negative internal thoughts rather than external forces like societal pressures or luck.
+          Fostering creativity, love, authenticity, and free will are common avenues that help move you toward transformation.
+        </p> <br />
 
         <h3> {psychodynamicTitle} </h3>
         <p> {psychodynamic} </p> <br />
@@ -56,7 +74,3 @@ class Definitions extends Component {
       </div>
     );
   }
-
-}
-
-export default Definitions;
