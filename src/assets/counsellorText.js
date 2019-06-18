a70abcb0648fb7425e439e6c0911e8fca4ab6227
felixjtdb <felixjtdb@gmail.com>
2019-06-19 00:48:53 +0100

    Added images data for Counsellors

---
+++ src/assets/counsellorText.js
@@ -0,0 +1,102 @@
+export const nicky = {
+  intro: "I am a qualified Cognitive Behavioural therapist with a particular interest since 2012 in Acceptance and \
+  Commitment Therapy.",
+
+  main: "I am passionate about helping clients to make sense of their world, tailoring techniques and interventions \
+  to create an effective therapy. It is a partnership and we can work as fast or slow as you like, this can \
+  mean weekly, fortnightly or monthly. Initially it is helpful to see each other weekly for a period as this \
+  helps to keep up the momentum of therapy.\n\n\
+  I have an open minded compassionate approach and I do not believe I am the expert who can tell you \
+  what changes to make, instead I am a trained professional who can sit alongside you and use my expertise \
+  to help you to work out what is blocking you from living the life you want.\n\n\
+  We will explore the links between thoughts, behaviour and mood, how we can often get caught up in our \
+  minds and are unable to break out of this state, it can also help to look at these patterns, and feel we have \
+  to a space to talk and feel heard. I have worked in private practice for over 10 years so have expereince of \
+  working with a range of emotional issues.\n\n\
+  I am a registered member of the BABCP and I abide by the BABCP ethical framework and all our \
+  sessions will be confidential. \n\n\
+  Please get in touch if you have any questions about working with me, I offer a no commitment 30 minute \
+  telephone consultation – email or text me to arrange a convenient time. \n\
+  Sessions are 50 minutes long and I charge £45 a session, I require 48 hours notice for cancellation of a \
+  session otherwise the full fee will be payable. Payment is due on the day the session is held. Payments \
+  are made by bank transfer or cash.",
+
+  training: ["Diploma in Cognitive Behavioural Therapy"],
+
+}
+
+export const julian = {
+  intro: "I offer both short and long-term working that can be tailored to your own needs. I work with Adults and \
+  Young People. I can offer experience of working in Schools, Colleges, Healthcare, as well as in the Community and \
+  in Private Practice.",
+
+  main: "My work is Integrative, which means I can use different ways of working to help you explore difficult or \
+  unhelpful aspects of your life that you would like to change, or better understand. \
+  I can work with an extensive variety of Emotional and Behavioral difficulties that include; Relationship Challenges \
+  (Young People and Adults), Work Stress, Low Confidence/Self Esteem, Anxiety and Panic, Living with ASD, Bereavement \
+  and Loss, Divorce/Separation, Learning Difficulties, Positive Parenting and Anger management.",
+
+  training: [
+    "BACP Certificate of Proficiency",
+    "CPCAB Diploma in Therapeutic Counselling",
+    "CPCAB Understanding Substance Misuse",
+    "CPCAB Certificate in Counselling Skills",
+    "CPCAB Certificate in Counselling Studies"
+  ]
+}
+
+export const julia = {
+  intro: "I am a BACP Registered counsellor/psychotherapist with extensive experience \
+  in helping clients who are struggling with anxiety, depression, bereavement, loss, \
+  relationships, identity issues, history of abuse, and self esteem issues, in addition \
+  to offering general counselling. I provide both time-limited counselling and longer-term \
+  therapy to adults and young people aged 16+.",
+
+  main: "My approach to counselling is integrative, drawing on a number of different theoretical models, \
+  predominantly existential and person-centred. The existential approach focuses on understanding how you \
+  make meaning in your life and what your priorities and values are, so that you can work out what you want \
+  in life and feel more confident in making choices. Person-centred means that the counselling process is led by \
+  you, and that we work together with what you bring and at your pace. The relationship between you and me, as your \
+  counsellor, is key to the work and provides a safe and supportive space for you to explore yourself and your \
+  experience without judgement or criticism.",
+
+  training: [
+    "MA in Counselling and Psychotherapy",
+    "Post-Graduate Diploma in Counselling",
+    "Certificate in Counselling Skills",
+    "Basic Counselling Skills, modules 1 and 2",
+    "BA Philosophy, Psychology and English"
+  ]
+}
+
+export const elizabeth = {
+  intro: "I am an experienced therapist and have worked with individuals and families both privately and within \
+  the NHS IAPT Mental Health Services. I offer therapy for depression and anxiety disorders, panic disorder, social \
+  phobia, post traumatic stress disorder, generalised anxiety disorder and other.",
+
+  main: "Fully insured with 10+ years of post qualification experience working within the community, for employment \
+  support organisations, and statutory services including NHS.",
+
+  training: [
+    "Qualified therapeutic Integrative Counsellor and CBT Therapist.",
+    "Professional registration with the BACP and BABCP umbrella regulatory bodies."
+  ]
+}
+
+export const anna = {
+  intro: "I am an Integrative Counsellor and I can tailor my counselling modality to best suit the client, \
+  I have a Diploma in Cognitive Behaviour Therapy (CBT) and Mindfulness practice. I work with clients on a \
+  short term and long term basis.  I have experience of working in secondary mental health services for \
+  15 years and I have worked in primary care in an IAPT Service for 4 years.",
+
+  main: "I have been working in private practice for the last 7 years and work with low self- esteem, \
+  loss bereavement, depression and anxiety OCD, self harm and sexual abuse, work related stress, \
+  relationship difficulties, identity and addictions.",
+
+  training: [
+    "Level 5 CPCAB Diploma in Cognitive Behavioural Therapeutic skills",
+    "Level 4 CPCAB Diploma in Therapeutic Counselling",
+    "Level 3 Certificate in Person-centred counselling",
+    "Introduction to Counselling"
+  ]
+}
